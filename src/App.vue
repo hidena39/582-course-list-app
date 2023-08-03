5020191b7a6a7d26dc1dd88908910a71177cfcc9
2295439 <2295439@edu.vaniercollege.qc.ca>
2023-08-03 09:44:41 -0400

    Aug03morning

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,16 @@
   <!-- <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   <h1>Course List</h1>
-  <!-- <p>{{ count }}</p>-->
+  <h2>Total Course Number</h2>
+  <p>{{ count }}</p>
   <div class="parent">
-    <CourseItem v-for="course of couses" :key="course.id" />
+    <CourseItem
+      v-for="course in courses"
+      :course="course"
+      :key="course.id"
+      @count-to-parent="addOne"
+      @count-from-parent="removeOne"
+    />
   </div>
 </template>
 
@@ -14,9 +21,57 @@ import CourseItem from "./components/CourseItem.vue";
 
 export default {
   name: "App",
+
   data() {
     return {
-      //  count: 0,
+      count: 0,
+      courses: [
+        {
+          name: "Web Design Specialist",
+          credit: "20",
+          id: "0",
+          hours: "60",
+          description: "This is a intensive web design course",
+          teacher: "Peter",
+          full: "false",
+        },
+        {
+          name: "Web Design Specialist",
+          credit: "20",
+          id: "1",
+          hours: "60",
+          description: "This is a intensive web design course",
+          teacher: "Peter",
+          full: "false",
+        },
+        {
+          name: "Web Design Specialist",
+          credit: "20",
+          id: "2",
+          hours: "60",
+          description: "This is a intensive web design course",
+          teacher: "Peter",
+          full: "false",
+        },
+        {
+          name: "Web Design Specialist",
+          credit: "20",
+          id: "3",
+          hours: "60",
+          description: "This is a intensive web design course",
+          teacher: "Peter",
+          full: "false",
+        },
+        {
+          name: "Web Design Specialist",
+          credit: "20",
+          id: "4",
+          hours: "60",
+          description: "This is a intensive web design course",
+          teacher: "Peter",
+          full: "false",
+        },
+      ],
     };
   },
   components: {
@@ -24,9 +79,13 @@ export default {
     CourseItem,
   },
   methods: {
-    // addOne() {
-    //   this.count++;
-    // },
+    addOne() {
+      this.count++;
+    },
+    removeOne() {
+      this.count--;
+    },
+ 
   },
 };
 </script>
--- src/components/CourseItem.vue
+++ src/components/CourseItem.vue
@@ -1,117 +1,60 @@
 <template>
   <div class="children">
-    <div>Total Course Number</div>
-    <div>{{ count }}</div>
     <div>Course Name:</div>
-    <div>{{ courses.name }}</div>
+    <div>{{ course.name }}</div>
     <div>Credit:</div>
-    <div>{{ courses.credit }}</div>
+    <div>{{ course.credit }}</div>
     <div>Course ID:</div>
-    <div>{{ courses.id }}</div>
+    <div>{{ course.id }}</div>
     <div>Total Hours:</div>
-    <div>{{ courses.hours }}</div>
+    <div>{{ course.hours }}</div>
     <div>Course Description:</div>
-    <div>{{ courses.description }}</div>
+    <div>{{ course.description }}</div>
     <div>Instructor:</div>
-    <div>{{ courses.teacher }}</div>
+    <div>{{ course.teacher }}</div>
     <div>Course Availability:</div>
-    <div>{{ courses.full }}</div>
-    <!-- <button @click="outerAdd">Add</button> -->
-    <button @click="innerAdd">Add</button>
-    <button @click="innerRemove">Remove</button>
+    <div>{{ course.full }}</div>
+    <button @click="outerAdd">Add</button>
+    <button @click="outerRemove">Remove</button>
+    <!-- <button @click="innerAdd">Add</button>
+    <button @click="innerRemove">Remove</button> -->
   </div>
 </template>
 
 <script>
 export default {
   name: "CourseItem",
+  props: {
+    course: {
+      type: Object,
+      required: true,
+    },
+  },
   data() {
     return {
       count: 0,
-      courses: [
-        {
-          name: "Web Design Specialist",
-          credit: "20",
-          id: "0",
-          hours: "60",
-          description: "This is a intensive web design course",
-          teacher: "Peter",
-          full: "false",
-        },
-        {
-          name: "Web Design Specialist",
-          credit: "20",
-          id: "1",
-          hours: "60",
-          description: "This is a intensive web design course",
-          teacher: "Peter",
-          full: "false",
-        },
-        {
-          name: "Web Design Specialist",
-          credit: "20",
-          id: "2",
-          hours: "60",
-          description: "This is a intensive web design course",
-          teacher: "Peter",
-          full: "false",
-        },
-        {
-          name: "Web Design Specialist",
-          credit: "20",
-          id: "3",
-          hours: "60",
-          description: "This is a intensive web design course",
-          teacher: "Peter",
-          full: "false",
-        },
-        {
-          name: "Web Design Specialist",
-          credit: "20",
-          id: "4",
-          hours: "60",
-          description: "This is a intensive web design course",
-          teacher: "Peter",
-          full: "false",
-        },
-        {
-          name: "Web Design Specialist",
-          credit: "20",
-          id: "5",
-          hours: "60",
-          description: "This is a intensive web design course",
-          teacher: "Peter",
-          full: "false",
-        },
-        {
-          name: "Web Design Specialist",
-          credit: "20",
-          id: "6",
-          hours: "60",
-          description: "This is a intensive web design course",
-          teacher: "Peter",
-          full: "false",
-        },
-      ],
     };
   },
   methods: {
-    innerAdd() {
-      this.count++;
-    },
-    innerRemove() {
-      this.count--;
-    },
-    // outerAdd() {
-    //   this.$emit("count-to-parent");
+    // innerAdd() {
+    //   this.count++;
     // },
+    // innerRemove() {
+    //   this.count--;
+    // },
+    outerAdd() {
+      this.$emit("count-to-parent");
+    },
+    outerRemove() {
+      this.$emit("count-from-parent");
+    },
   },
 };
 </script>
 
 <style lang="scss">
 .children {
-  width: 200px;
+  width: 400px;
   height: 300px;
   border: 1px solid black;
   word-wrap: break-word;
